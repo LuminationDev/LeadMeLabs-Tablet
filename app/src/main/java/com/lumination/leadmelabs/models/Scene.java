05f53be394e6b6fe30a0ff49c47bef762c4c775f
Edward Michael Coad <ecoad@lumination.com.au>
2022-03-31 15:23:04 +1030

    Implemented data binding for scenes

--- app/src/main/java/com/lumination/leadmelabs/models/Scene.java
+++ app/src/main/java/com/lumination/leadmelabs/models/Scene.java
@@ -1,15 +1,20 @@
 package com.lumination.leadmelabs.models;
 
+import androidx.lifecycle.MutableLiveData;
+
 public class Scene {
     public String name;
     public int number;
     public int value;
-//    public Drawable icon;
+    public MutableLiveData<Integer> icon;
+    public MutableLiveData<Boolean> isActive;
 
     public Scene(String name, int number, int value) {
         this.name = name;
         this.number = number;
         this.value = value;
-//        this.icon = icon;
+
+        icon = new MutableLiveData<>();
+        isActive = new MutableLiveData<>(false);
     }
 }
