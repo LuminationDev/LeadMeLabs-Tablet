2052b5bc888f9afc6e5b0d8e212ad4588ce5ca67
Edward Michael Coad <ecoad@lumination.com.au>
2024-02-01 14:04:24 +1030

    Tidy up audio section
    Handle audio for non-vr stations

--- app/src/main/java/com/lumination/leadmelabs/models/applications/details/Actions.java
+++ app/src/main/java/com/lumination/leadmelabs/models/applications/details/Actions.java
@@ -1,8 +1,11 @@
 package com.lumination.leadmelabs.models.applications.details;
 
+import org.json.JSONObject;
+
 public class Actions {
     public String name;
     public String trigger;
+    public JSONObject extra;
 
     public Actions(String name, String trigger) {
         this.name = name;
