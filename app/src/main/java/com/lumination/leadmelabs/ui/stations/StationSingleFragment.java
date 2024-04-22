eeee2492095d28364ef481347b91b80d242e0eed
Edward Michael Coad <ecoad@lumination.com.au>
2024-04-22 09:48:29 +0930

    Show idle button
    - Revert hiding the idle button from last release

--- app/src/main/java/com/lumination/leadmelabs/ui/stations/StationSingleFragment.java
+++ app/src/main/java/com/lumination/leadmelabs/ui/stations/StationSingleFragment.java
@@ -206,13 +206,12 @@ public class StationSingleFragment extends Fragment {
             fragment.loadFragment(HelpPageFragment.class, "help", null);
         });
 
-        //TODO not for current release
-        //Run the identify flow
-//        FlexboxLayout identify = view.findViewById(R.id.identify_button);
-//        identify.setOnClickListener(v -> {
-//            List<Station> stations = Collections.singletonList(binding.getSelectedStation());
-//            Identifier.identifyStations(stations);
-//        });
+//        Run the identify flow
+        FlexboxLayout identify = view.findViewById(R.id.identify_button);
+        identify.setOnClickListener(v -> {
+            List<Station> stations = Collections.singletonList(binding.getSelectedStation());
+            Identifier.identifyStations(stations);
+        });
 
         Button button = view.findViewById(R.id.enter_url);
         button.setOnClickListener(v ->
@@ -322,20 +321,12 @@ public class StationSingleFragment extends Fragment {
             trackStationEvent(SegmentConstants.Station_Status_Touch);
         });
 
-        //TODO not for current release
-//        Button idleMode = view.findViewById(R.id.idle_mode);
-//        idleMode.setOnClickListener(v -> {
-//            Station selectedStation = binding.getSelectedStation();
-//            String value = selectedStation.statusHandler.isStationIdle() ? "normal" : "idle";
-//
-//            NetworkService.sendMessage("Station," + selectedStation.id, "Station", "SetValue:idleMode:" + value);
-//        });
+        Button idleMode = view.findViewById(R.id.idle_mode);
+        idleMode.setOnClickListener(v -> {
+            Station selectedStation = binding.getSelectedStation();
+            String value = selectedStation.statusHandler.isStationIdle() ? "normal" : "idle";
 
-        //TODO only for current release
-        Button identify = view.findViewById(R.id.identify_button);
-        identify.setOnClickListener(v -> {
-            List<Station> stations = Collections.singletonList(binding.getSelectedStation());
-            Identifier.identifyStations(stations);
+            NetworkService.sendMessage("Station," + selectedStation.id, "Station", "SetValue:idleMode:" + value);
         });
 
         Button restartVr = view.findViewById(R.id.station_restart_vr);
